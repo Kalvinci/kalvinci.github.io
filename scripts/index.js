9336a2ec122a0bf9db0565e4c880fc3e50ce75c7
Kalvinci <kalyanasundharam97@gmail.com>
2019-08-17 23:59:29 +0530

    skill set icons now rotate

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,24 @@
 
 const about_me = ['Hi! I am', 'Kalyanasundharam Kathiresan&nbsp;&lt;kalvinci<i>&sol;</i>&gt;', 'A Computer Science engineer, a programmer, and a maverick.', 'I believe hard-work beats talent.', 'I also believe that people who are modest are cool.', 'I like Spider-man. (since 2004)', 'I\'m a huge fan of Tom Cruise.', 'Even numbers rock. (please pay attention)', 'Most importantly, I love simplicity.', 'Blaah.. blaaah... blaaaaaah......', 'Scroll to read me!'];
 
-var header = document.querySelector('header');
+const header = document.querySelector('header');
 
-var page_0 = document.querySelector('.page-0');
+const page_0 = document.querySelector('.page-0');
 
-var profilePic = document.querySelector('#profile-pic');
+const profilePic = document.querySelector('#profile-pic');
 
-var header_profilePic = document.querySelector('#header-profile-pic');
+const header_profilePic = document.querySelector('#header-profile-pic');
 
-var page_0_rect = page_0.getBoundingClientRect();
+const type_out_node_list = document.getElementsByClassName('type-out-text');
 
-var initial_rotation_count = 0;
+const skill_icon_list = document.querySelectorAll('.skill-set > div');
 
-var type_out_node_list = document.getElementsByClassName('type-out-text');
+let page_0_rect = page_0.getBoundingClientRect();
+
+let initial_rotation_count = 0;
 
 window.onload = function() {
-    var inital_load_disk_rotation = setInterval(function() {
+    let inital_load_disk_rotation = setInterval(function() {
         profilePic.style['transform'] = 'rotate('+initial_rotation_count+'deg)';
         initial_rotation_count += 45;
         if (initial_rotation_count >= 1845) {
@@ -25,9 +27,9 @@ window.onload = function() {
         }
     }, 150);
     
-    var i=0, y=0, k=0;
+    let i=0, y=0, k=0;
     
-    var type_out_sentence = setInterval(function() {
+    let type_out_sentence = setInterval(function() {
         type_out_node_list[i].innerHTML = about_me[i];
         i++;
         if (i == about_me.length-2) {
@@ -47,7 +49,7 @@ window.onload = function() {
     }, 400);
 }
 
-var rotation_count = 0;
+let rotation_count = 0;
 
 window.addEventListener('scroll', function() {
     if (page_0_rect.top >= 0) {
@@ -59,10 +61,28 @@ window.addEventListener('scroll', function() {
     }
     page_0_rect = page_0.getBoundingClientRect();
     profilePic.style['transform'] = 'rotate('+rotation_count+'deg)';
-    header_profilePic.style['transform'] = 'rotate('+rotation_count+'deg)';
-    if (page_0_rect.bottom <= -50) {
-        header.style.display = 'grid';
-    } else {
-        header.style.display = 'none';
-    }
+//    header_profilePic.style['transform'] = 'rotate('+rotation_count+'deg)';
+//    if (page_0_rect.bottom <= -50) {
+//        header.style.display = 'grid';
+//    } else {
+//        header.style.display = 'none';
+//    }
+});
+
+let skill_icon_rotation = undefined;
+let skill_rotation_count = 0;
+
+skill_icon_list.forEach(function(element) {
+    element.addEventListener('mouseover', function() {
+        skill_icon_rotation = setInterval(function() {
+            skill_rotation_count += 20;
+            element.children[0].style['transform'] = 'rotateY('+skill_rotation_count+'deg)';
+        }, 40);
+    });
+    
+    element.addEventListener('mouseout', function() {
+        skill_rotation_count = 0;
+        element.children[0].style['transform'] = 'rotateY('+skill_rotation_count+'deg)';
+        clearInterval(skill_icon_rotation);
+    });
 });
